a97cc71ffd6c986aecd8bb2e6e35e53e2d381c4d
Nathan Keller <keller.nathan@gmail.com>
2020-01-28 16:37:23 -0700

    Heroku push not working. Trying a branch merge

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -7,7 +7,7 @@ class SheltersController < ApplicationController
   end
 
   def show
-    @shelter = Shelter.find(params[:id])
     require "pry"; binding.pry
+    @shelter = Shelter.find(params[:id])
   end
 end
