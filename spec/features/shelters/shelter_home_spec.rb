40fcd2c8a44950ec7192622acb8d02e20eb3c1bd
Raymond Nguyen <rayknguyen@gmail.com>
2020-01-28 10:44:59 -0700

    Add view for shelters

--- spec/features/shelters/shelter_home_spec.rb
+++ spec/features/shelters/shelter_home_spec.rb
@@ -1,23 +1,21 @@
 require "rails_helper"
 
-RSpec.describe Shelter, type: :model do 
-  describe "as a visitor" do 
+  RSpec.describe "as a visitor" do 
     it "can see the name of each shelter" do
-      shelter_1 = Shelter.new(name: "Denver Shelter", 
-                              address: "123 fake st.",
-                              city: "Denver",
-                              state: "CO",
-                              zip: 80018)
+      shelter_1 = Shelter.create(name: "Denver Shelter", 
+                                  address: "123 fake st.",
+                                  city: "Denver",
+                                  state: "CO",
+                                  zip: 80018)
 
-      shelter_2 = Shelter.new(name: "Aurora Shelter",
-                              address: "800 Some Ave",
-                              city: "Aurora",
-                              state: "CO",
-                              zip: 80249)
+      shelter_2 = Shelter.create(name: "Aurora Shelter",
+                                  address: "800 Some Ave",
+                                  city: "Aurora",
+                                  state: "CO",
+                                  zip: 80249)
       visit "/shelters"
 
       expect(page).to have_content(shelter_1.name)
       expect(page).to have_content(shelter_2.name)
-    end
   end
 end
