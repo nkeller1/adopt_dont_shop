29b846e2d27df4d651653a1450b3f018b032e047
Nathan Keller <keller.nathan@gmail.com>
2020-01-29 08:26:38 -0700

    Adjust seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,18 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Shelter.destroy_all
+shelter_1 = Shelter.create(
+                     name: "Mike's Shelter",
+                     address: "1331 17th Street",
+                     city: "Denver",
+                     state: "CO",
+                     zip: 80202
+                   )
+shelter_2 = Shelter.create(
+                      name: "Meg's Shelter",
+                      address: "150 Main Street",
+                      city: "Hershey",
+                      state: "PA",
+                      zip: 17033
+                    )
