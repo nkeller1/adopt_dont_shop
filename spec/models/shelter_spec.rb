292b09f5be069e850014af3db350230f2d0bf79b
Raymond Nguyen <rayknguyen@gmail.com>
2020-01-29 11:11:29 -0700

    Add test for relationship for shelter and pets

--- spec/models/shelter_spec.rb
+++ spec/models/shelter_spec.rb
@@ -2,10 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Shelter, type: :model do 
   describe "validations" do
-    it { should validate_presence_of :name}
-    it { should validate_presence_of :address}
-    it { should validate_presence_of :city}
-    it { should validate_presence_of :state}
-    it { should validate_presence_of :zip}
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :address }
+    it { should validate_presence_of :city }
+    it { should validate_presence_of :state }
+    it { should validate_presence_of :zip }
+  end
+  describe "relationship" do
+    it { should have_many :pets}
   end
 end
