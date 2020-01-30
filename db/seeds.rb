f43d8bcc019adae754970458eb8bcb53ff2f6cd4
Nathan Keller <keller.nathan@gmail.com>
2020-01-30 08:16:24 -0700

    Adjust seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,17 +3,17 @@
 #
 # Examples:
 Shelter.destroy_all
-shelter_1 = Shelter.create(
-                     name: "Mike's Shelter",
-                     address: "1331 17th Street",
-                     city: "Denver",
-                     state: "CO",
-                     zip: 80202
-                   )
-shelter_2 = Shelter.create(
-                      name: "Meg's Shelter",
-                      address: "150 Main Street",
-                      city: "Hershey",
-                      state: "PA",
-                      zip: 17033
-                    )
+Shelter.create(
+             name: "Mike's Shelter",
+             address: "1331 17th Street",
+             city: "Denver",
+             state: "CO",
+             zip: 80202
+           )
+Shelter.create(
+            name: "Meg's Shelter",
+            address: "150 Main Street",
+            city: "Hershey",
+            state: "PA",
+            zip: 17033
+          )
