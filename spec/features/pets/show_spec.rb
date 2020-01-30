32f5a095cdba1420d83a97bde333e62c8d31d191
Raymond Nguyen <rayknguyen@gmail.com>
2020-01-30 11:23:21 -0700

    Add show spec for pets

---
+++ spec/features/pets/show_spec.rb
@@ -0,0 +1,35 @@
+require "rails_helper"
+
+RSpec.describe "as a visitor", type: :feature do
+  describe "on show page" do 
+    before(:each) do
+      @shelter_1 = Shelter.create(  name: "Denver Shelter", 
+                                    address: "123 fake st.",
+                                    city: "Denver",
+                                    state: "CO",
+                                    zip: 80018)
+      @pet_1 = Pet.create(          image: "https://images.theconversation.com/files/291243/original/file-20190906-175705-cjptgw.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1000&fit=clip",
+                                    name: "Lucii",
+                                    approximate_age: 6000,
+                                    sex: "F",
+                                    shelter: @shelter_1)
+                                    
+      @pet_2 = Pet.create(          image: "https://www.thoughtco.com/thmb/U7DKDGmDj_zH7Sqz5JYv6JPomQs=/640x603/filters:no_upscale():max_bytes(150000):strip_icc()/dinomeme5-56a256bb3df78cf772748c40.jpg",
+                                    name: "Taco",
+                                    approximate_age: 300,
+                                    sex: "M",
+                                    shelter: @shelter_1)
+    end
+    
+    it "can see the that pet and its information" do
+      visit "/pets/#{@pet_1.id}"
+
+      expect(page).to have_css(@pet_1.image) 
+      expect(page).to have_content(@pet_1.name) 
+      expect(page).to have_content(@pet_1.approximate_age) 
+      expect(page).to have_content(@pet_1.sex)
+      expext(page).to have_content(@pet_1.adoptable)
+    end
+  end
+end
+
