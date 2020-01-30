6ce3b89cbecd70517f3aaba284c208c88ff159b6
Raymond Nguyen <rayknguyen@gmail.com>
2020-01-30 10:23:31 -0700

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,19 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/shelters", to: "shelters#index"
-  get "/shelters/new", to: "shelters#new"
-  get "/shelters/:id", to: "shelters#show"
-  post "/shelters", to: "shelters#create"
-  get "/shelters/:id/edit", to: "shelters#edit"
-  patch "/shelters/:id", to: "shelters#update"
-  delete "/shelters/:id", to: "shelters#destroy"
+  get '/', to: "welcome#index"
 
-  get "/shelters/:shelter_id/pets", to: "shelters#index"
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # get "/shelters", to: "shelters#index"
+  # get "/shelters/new", to: "shelters#new"
+  # get "/shelters/:id", to: "shelters#show"
+  # post "/shelters", to: "shelters#create"
+  # get "/shelters/:id/edit", to: "shelters#edit"
+  # patch "/shelters/:id", to: "shelters#update"
+  # delete "/shelters/:id", to: "shelters#destroy"
+  
+  resources :shelters 
+
+  get "/shelters/:shelter_id/pets", to: "pets#index"
 
   get "/pets", to: "pets#index"
 
