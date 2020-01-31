ac9a2137ee27447779828852b53cbe1528146057
Raymond Nguyen <rayknguyen@gmail.com>
2020-01-31 09:19:30 -0700

    Update index spec

--- spec/features/pets/index_spec.rb
+++ spec/features/pets/index_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe "on the index page", type: :feature do
                                     address: "123 fake st.",
                                     city: "Denver",
                                     state: "CO",
-                                    zip: 80018)
+                                    zip: "80018")
       @pet_1 = Pet.create(          image: "https://images.theconversation.com/files/291243/original/file-20190906-175705-cjptgw.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1000&fit=clip",
                                     name: "Lucii",
                                     approximate_age: 6000,
