949122c617b2d6bcc8ac65fae5b0bbf85b82cafb
Raymond Nguyen <rayknguyen@gmail.com>
2020-01-31 09:25:09 -0700

    Add index spec for pets

--- spec/features/shelters/index_spec.rb
+++ spec/features/shelters/index_spec.rb
@@ -7,19 +7,36 @@ require "rails_helper"
                                     address: "123 fake st.",
                                     city: "Denver",
                                     state: "CO",
-                                    zip: 80018)
+                                    zip: "80018")
   
         @shelter_2 = Shelter.create(name: "Aurora Shelter",
                                     address: "800 Some Ave",
                                     city: "Aurora",
                                     state: "CO",
-                                    zip: 80249)
+                                    zip: "80249")
       end
       it "can see the name of each shelter" do
         visit "/shelters"
 
         expect(page).to have_content(@shelter_1.name)
         expect(page).to have_content(@shelter_2.name)
+      end
+      it "has links to shelters" do
+        visit "/shelters"
+
+        expect(page).to have_link(@shelter_1.name)
+
+        click_link(@shelter_1.name)
+        
+        expect(current_path).to eq("/shelters/#{@shelter_1.id}")
+
+        visit "/shelters"
+
+        expect(page).to have_link(@shelter_2.name)
+
+        click_link(@shelter_2.name)
+        
+        expect(current_path).to eq("/shelters/#{@shelter_2.id}")
       end
   end
 end
