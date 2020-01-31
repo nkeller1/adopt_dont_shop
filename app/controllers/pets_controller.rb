a83e996347f667ecc1e09936ef1bd32522a6a5ee
Raymond Nguyen <rayknguyen@gmail.com>
2020-01-31 09:24:38 -0700

    Add method for pets

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -7,4 +7,24 @@ class PetsController < ApplicationController
       @pets = Pet.all
     end
   end
+
+  def show
+    @pet = Pet.find(params[:id])
+  end
+
+  def new
+    @shelter_id = params[:shelter_id]
+  end
+
+  def create
+    shelter = Shelter.find(params[:shelter_id])
+    pet = shelter.pets.create(pet_params)
+    redirect_to "/shelters/#{shelter.id}/pets"
+  end
+
+    private
+
+  def pet_params
+    params.permit(:image, :name, :approximate_age, :sex, :description, :adoptable)
+  end
 end
