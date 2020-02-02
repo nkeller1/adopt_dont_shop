fac03a53244addab23b12eeaee2b8833df9964a5
Raymond Nguyen <rayknguyen@gmail.com>
2020-02-02 09:54:18 -0700

    Add crud method for pets

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -22,6 +22,21 @@ class PetsController < ApplicationController
     redirect_to "/shelters/#{shelter.id}/pets"
   end
 
+  def edit
+    @pet = Pet.find(params[:id])
+  end
+
+  def update
+    pet = Pet.find(params[:id])
+    pet.update(pet_params)
+    redirect_to "/pets/#{pet.id}"
+  end
+
+  def destroy
+    Pet.destroy(params[:id])
+    redirect_to "/pets"
+  end
+
     private
 
   def pet_params
