63d67d23b19a804e620854cfb5d36a830b7767e2
Raymond Nguyen <rayknguyen@gmail.com>
2020-02-02 09:50:35 -0700

    Update test

--- spec/features/pets/index_spec.rb
+++ spec/features/pets/index_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe "on the index page", type: :feature do
                                     city: "Denver",
                                     state: "CO",
                                     zip: "80018")
+
       @pet_1 = Pet.create(          image: "https://images.theconversation.com/files/291243/original/file-20190906-175705-cjptgw.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1000&fit=clip",
                                     name: "Lucii",
                                     approximate_age: 6000,
@@ -34,20 +35,24 @@ RSpec.describe "on the index page", type: :feature do
     end
     it "can see all pets that belong to that shelter" do
 
-    visit "/shelters/#{@shelter_1.id}/pets"
+      visit "/shelters/#{@shelter_1.id}/pets"
 
-    expect(page).to have_content("Current Shelter: #{@shelter_1.name}")
-  
-    expect(page).to have_content(@shelter_1.name)
-    expect(page).to have_css("img[src*='#{@pet_1.image}']")
-    expect(page).to have_link(@pet_1.name)
-    expect(page).to have_content("Age: #{@pet_1.approximate_age}")
-    expect(page).to have_content("Sex: #{@pet_1.sex}")
-  
-    expect(page).to have_css("img[src*='#{@pet_2.image}']")
-    expect(page).to have_content(@pet_2.name)
-    expect(page).to have_content("Age: #{@pet_2.approximate_age}")
-    expect(page).to have_content("Sex: #{@pet_2.sex}")                             
+      expect(page).to have_content("Current Shelter: #{@shelter_1.name}")
+    
+      expect(page).to have_content(@shelter_1.name)
+      expect(page).to have_css("img[src*='#{@pet_1.image}']")
+      expect(page).to have_link(@pet_1.name)
+      expect(page).to have_content("Age: #{@pet_1.approximate_age}")
+      expect(page).to have_content("Sex: #{@pet_1.sex}")
+    
+      expect(page).to have_css("img[src*='#{@pet_2.image}']")
+      expect(page).to have_content(@pet_2.name)
+      expect(page).to have_content("Age: #{@pet_2.approximate_age}")
+      expect(page).to have_content("Sex: #{@pet_2.sex}")                             
+    end
+    it "see count of numbers of pets in this shelter" do
+      visit "/shelters/#{@shelter_1.id}/pets"
+      expect(page).to have_content(@shelter_1.pets.count)
     end
   end
 end
