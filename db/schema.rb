f5bbf2a7679ac1e414cc90605969f15111692464
Raymond Nguyen <rayknguyen@gmail.com>
2020-02-02 09:52:06 -0700

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200131004942) do
+ActiveRecord::Schema.define(version: 20200201212624) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,8 +23,8 @@ ActiveRecord::Schema.define(version: 20200131004942) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "shelter_id"
-    t.string "adoptable", default: "yes"
     t.string "description"
+    t.string "adoptable", default: "Adoptable"
     t.index ["shelter_id"], name: "index_pets_on_shelter_id"
   end
 
